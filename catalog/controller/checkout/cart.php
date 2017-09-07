c3340a10e81b8756a5d5fc19e1a457eade9955e8
FelixFox0 <ssseee795@gmail.com>
2017-09-07 18:51:10 +0300

    add modal

--- catalog/controller/checkout/cart.php
+++ catalog/controller/checkout/cart.php
@@ -405,6 +405,130 @@ class ControllerCheckoutCart extends Controller {
 		$this->response->addHeader('Content-Type: application/json');
 		$this->response->setOutput(json_encode($json));
 	}
+        
+        public function popup() {
+		$this->load->language('checkout/cart');
+
+		$json = array();
+
+		if (isset($this->request->post['product_id'])) {
+			$product_id = (int)$this->request->post['product_id'];
+		} else {
+			$product_id = 0;
+		}
+
+		$this->load->model('catalog/product');
+
+		$product_info = $this->model_catalog_product->getProduct($product_id);
+
+		if ($product_info) {
+                    /*
+			if (isset($this->request->post['quantity']) && ((int)$this->request->post['quantity'] >= $product_info['minimum'])) {
+				$quantity = (int)$this->request->post['quantity'];
+			} else {
+				$quantity = $product_info['minimum'] ? $product_info['minimum'] : 1;
+			}
+
+			if (isset($this->request->post['option'])) {
+				$option = array_filter($this->request->post['option']);
+			} else {
+				$option = array();
+			}
+
+			$product_options = $this->model_catalog_product->getProductOptions($this->request->post['product_id']);
+
+			foreach ($product_options as $product_option) {
+				if ($product_option['required'] && empty($option[$product_option['product_option_id']])) {
+					$json['error']['option'][$product_option['product_option_id']] = sprintf($this->language->get('error_required'), $product_option['name']);
+				}
+			}
+
+			if (isset($this->request->post['recurring_id'])) {
+				$recurring_id = $this->request->post['recurring_id'];
+			} else {
+				$recurring_id = 0;
+			}
+
+			$recurrings = $this->model_catalog_product->getProfiles($product_info['product_id']);
+
+			if ($recurrings) {
+				$recurring_ids = array();
+
+				foreach ($recurrings as $recurring) {
+					$recurring_ids[] = $recurring['recurring_id'];
+				}
+
+				if (!in_array($recurring_id, $recurring_ids)) {
+					$json['error']['recurring'] = $this->language->get('error_recurring_required');
+				}
+			}
+*/
+			if (!$json) {
+                                $json['product_info'] = $product_info;
+//				$this->cart->add($this->request->post['product_id'], $quantity, $option, $recurring_id);
+
+				$json['success'] = sprintf($this->language->get('text_success'), $this->url->link('product/product', 'product_id=' . $this->request->post['product_id'], false, $this->session->data['country_code'], $this->session->data['language_name']), $product_info['name'], $this->url->link('checkout/cart', '', false, $this->session->data['country_code'], $this->session->data['language_name']));
+
+				// Unset all shipping and payment methods
+//				unset($this->session->data['shipping_method']);
+//				unset($this->session->data['shipping_methods']);
+//				unset($this->session->data['payment_method']);
+//				unset($this->session->data['payment_methods']);
+
+				// Totals
+//				$this->load->model('extension/extension');
+//
+//				$totals = array();
+//				$taxes = $this->cart->getTaxes();
+//				$total = 0;
+		
+				// Because __call can not keep var references so we put them into an array. 			
+//				$total_data = array(
+//					'totals' => &$totals,
+//					'taxes'  => &$taxes,
+//					'total'  => &$total
+//				);
+
+				// Display prices
+				/*if ($this->customer->isLogged() || !$this->config->get('config_customer_price')) {
+					$sort_order = array();
+
+					$results = $this->model_extension_extension->getExtensions('total');
+
+					foreach ($results as $key => $value) {
+						$sort_order[$key] = $this->config->get($value['code'] . '_sort_order');
+					}
+
+					array_multisort($sort_order, SORT_ASC, $results);
+
+					foreach ($results as $result) {
+						if ($this->config->get($result['code'] . '_status')) {
+							$this->load->model('total/' . $result['code']);
+
+							// We have to put the totals in an array so that they pass by reference.
+							$this->{'model_total_' . $result['code']}->getTotal($total_data);
+						}
+					}
+
+					$sort_order = array();
+
+					foreach ($totals as $key => $value) {
+						$sort_order[$key] = $value['sort_order'];
+					}
+
+					array_multisort($sort_order, SORT_ASC, $totals);
+				}
+*/
+                                $json['text_cart'] = $this->language->get('text_back');
+//				$json['total'] = sprintf($this->language->get('text_items'), $this->cart->countProducts() + (isset($this->session->data['vouchers']) ? count($this->session->data['vouchers']) : 0), $this->currency->format($total, $this->session->data['currency']));
+			} else {
+				$json['redirect'] = str_replace('&amp;', '&', $this->url->link('product/product', 'product_id=' . $this->request->post['product_id'], false, $this->session->data['country_code'], $this->session->data['language_name']));
+			}
+		}
+
+		$this->response->addHeader('Content-Type: application/json');
+		$this->response->setOutput(json_encode($json));
+	}
 
 	public function edit() {
 		$this->load->language('checkout/cart');
